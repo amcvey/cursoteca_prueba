7184190beeba928a2892713a164c4c1a14953773
Andres <andmcvey@gmail.com>
2015-07-13 12:16:09 -0400

    modifiying some things in the views for showing school

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -69,6 +69,6 @@ class SchoolsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def school_params
-      params.require(:school).permit(:name, :email, :rut_school, :company_name, :bank_account)
+      params.require(:school).permit(:name, :email, :rut_school, :company_name, :bank_account, :address)
     end
 end
